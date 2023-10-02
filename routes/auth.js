ea33d4ce88143c921d2bcea673b249980a7b498b
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-02 11:17:32 +0300

    finally sets up passport local auth

--- routes/auth.js
+++ routes/auth.js
@@ -10,47 +10,62 @@ const bcrypt = require('bcrypt');
 passport.use(new LocalStrategy({
     usernameField: 'email'
 },
-    function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        const verifyPassword = bcrypt.compare(password, user.password);
-        if (!verifyPassword) { return done(null, false); }
-        return done(null, user);
-      });
+    async function (username, password, done) {
+        console.log(`username: ${username}, password: ${password}`);
+        await User.findOne({ 'email': username }).then(async (user) => {
+            console.log(user);
+            if (!user) { return done(null, false); }
+            const verifyPassword = await bcrypt.compare(password, user.password);
+            if (!verifyPassword) { return done(null, false); }
+            return done(null, user);
+        }).catch((e) => {
+            if (e) { return done(e); }
+        });
     }
-  ));
+));
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user._id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 
 authRoute.post('/login', passport.authenticate('local'), (req, res) => {
     if (req.user) {
-        res.json({"message": "user successfully logged in", "user": req.user});
+        res.json({ "message": "user successfully logged in", "user": req.user });
     } else {
-        res.json({"message": "failed to login user"});
+        res.json({ "message": "failed to login user" });
     }
 });
 
 authRoute.post('/signup', async (req, res) => {
-    const { email, password, userName} = req.body;
+    const { email, password, userName } = req.body;
     if (!email && !password && !userName) {
-        res.status(400).json({"message": "please provide the required data for sign up"});
+        res.status(400).json({ "message": "please provide the required data for sign up" });
         return;
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
         "email": email,
-        "password": password,
+        "password": hashedPassword,
         "userName": userName,
     });
 
-    await user.save().then((newuser) => {
+    await user.save().then((newUser) => {
         console.log(newUser);
-        if (newUser){
-            res.status(201).json({"message": "user sign up successful", "user": newUser});
+        if (newUser) {
+            res.status(201).json({ "message": "user sign up successful", "user": newUser });
         } else {
-            res.status(400).json({"message": "failed to sign up user"});
+            res.status(400).json({ "message": "failed to sign up user" });
         }
     }).catch((e) => {
         console.log(e);
-        res.status(400).json({"message": "something went wrong", "error": e});
+        res.status(400).json({ "message": "something went wrong", "error": e });
     });
 
 });
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const passport = require('passport');
+const session = require('express-session');
+
+
 
 const app = express();
+
+app.use(session({
+    secret: 'trialSecretKey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api/v1/auth', authRoute);
 
 const port = process.env.PORT || 4001;
 
