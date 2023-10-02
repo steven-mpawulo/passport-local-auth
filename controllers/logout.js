c1d6e4523fb6674cf0115041a44fda44e26be072
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-02 12:24:31 +0300

    creates logout controller

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,8 +1,8 @@
-const login = (req, res, next) => {
+const logout = (req, res, next) => {
     req.logOut(function (err) {
         if (err) {next(err);}
         res.json({"message": "user logged out", "user": req.user});
     });
 }
 
-module.exports = login;
+module.exports = logOut;
