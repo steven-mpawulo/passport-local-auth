26818902b4b066d46e2157c1e4228f5328c53d50
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-02 12:20:50 +0300

    creates sign up controller

---
+++ controllers/signup.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const signup = async (req, res) => {
+    const { email, password, userName } = req.body;
+    if (!email && !password && !userName) {
+        res.status(400).json({ "message": "please provide the required data for sign up" });
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+        "email": email,
+        "password": hashedPassword,
+        "userName": userName,
+    });
+
+    await user.save().then((newUser) => {
+        console.log(newUser);
+        if (newUser) {
+            res.status(201).json({ "message": "user sign up successful", "user": newUser });
+        } else {
+            res.status(400).json({ "message": "failed to sign up user" });
+        }
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).json({ "message": "something went wrong", "error": e });
+    });
+}
+
+module.exports = signup;
--- routes/auth.js
+++ routes/auth.js
@@ -46,32 +46,7 @@ authRoute.post('/login', passport.authenticate('local'), (req, res) => {
     }
 });
 
-authRoute.post('/signup', async (req, res) => {
-    const { email, password, userName } = req.body;
-    if (!email && !password && !userName) {
-        res.status(400).json({ "message": "please provide the required data for sign up" });
-        return;
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-        "email": email,
-        "password": hashedPassword,
-        "userName": userName,
-    });
-
-    await user.save().then((newUser) => {
-        console.log(newUser);
-        if (newUser) {
-            res.status(201).json({ "message": "user sign up successful", "user": newUser });
-        } else {
-            res.status(400).json({ "message": "failed to sign up user" });
-        }
-    }).catch((e) => {
-        console.log(e);
-        res.status(400).json({ "message": "something went wrong", "error": e });
-    });
-
-});
+authRoute.post('/signup', );
 
 authRoute.post('/logout', (req, res, next) => {
     req.logOut(function (err) {
