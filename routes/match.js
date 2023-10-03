1dd37c27b5b65bcccc18148594804ada9da4f12f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-10-03 10:42:17 +0300

    adds get matches controller

--- routes/match.js
+++ routes/match.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createMatch = require('../controllers/match/createMatch');
 const deleteMatch = require('../controllers/match/deleteMatch');
+const getMatches = require('../controllers/match/getMatches');
 
 const matchRoute = express.Router();
 
 matchRoute.post('/:firstUserId/:secondUserId', createMatch);
 matchRoute.delete('/:matchId/:firstUserId/:secondUserId', deleteMatch);
+matchRoute.get('/', getMatches);
 
 module.exports = matchRoute;
